444071605a5e153712afb1cc253ce817cd98c9b6
Oscar Cabagné <OCabagne@outlook.com>
2022-05-11 02:57:28 -0500

    Actualización Problema 1

--- Practica4/Problema2/src/App.java
+++ Practica4/Problema2/src/App.java
@@ -1,26 +1,31 @@
 import javax.imageio.*;
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.awt.image.*;
 
 public class App
 {
     BufferedImage imagen = null;
+    BufferedImage copia = null;
     String path = "C://Users//Oscar//Desktop//ESCOM//10mo//Analisis de Algoritmos//Analisis-de-Algoritmos//Practica4/";
     int tamaño = 1024;
 
-    public boolean read()
+    public BufferedImage read()
     {
+        BufferedImage imagen = null;
         try 
         {
             File f_imagen = new File(path + "/imagen.bmp"); // Obtiene el archivo indicado en path
             imagen = ImageIO.read(f_imagen);    // Abre el archivo en formato BufferedImage
             System.out.println(">: Se ha leído correctamente la imagen.");  // Confirmación
-            return true;
+            tamaño = imagen.getWidth();
+            return imagen;
         } 
         catch (Exception e) 
         {
             System.out.println(">: Ocurrió un error: " + e);
-            return false;
+            return imagen;
         }
     }
 
@@ -35,81 +40,64 @@ public class App
             System.out.println(">: Ocurrió un error: " + e);
         }
     }
-
-    // convert BufferedImage to byte[]
-    public static byte[] toByteArray(BufferedImage bi, String format) throws IOException {
-
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        ImageIO.write(bi, format, baos);
-        byte[] bytes = baos.toByteArray();
-        return bytes;
-
-    }
-
-    // convert byte[] to BufferedImage
-    public static BufferedImage toBufferedImage(byte[] bytes)
-        throws IOException {
-
-        InputStream is = new ByteArrayInputStream(bytes);
-        BufferedImage bi = ImageIO.read(is);
-        return bi;
-
-    }
-
-    public void rotar()
-    {
-        try
-        {
-            File f_imagen = new File(path + "/copia.bmp"); // Obtiene el archivo indicado en path
-            BufferedImage nueva = ImageIO.read(f_imagen);    // Abre el archivo en formato BufferedImage
-
-            for (int h = 0; h < imagen.getHeight(); h++)
-            {
-                for (int w = 0; w < imagen.getWidth(); w++)
-                {
-                    int rgb = imagen.getRGB(h, w);
-                    nueva.setRGB(w, tamaño - h, rgb);     // Asigna el valor de imagen[w][h] a nueva[h][1023-w]
-                    //nueva.setRGB(w, tamaño-h, rgb);     // Asigna el valor de imagen[w][h] a nueva[h][1023-w]
-                }
-            }
-
-            ImageIO.write(nueva, "bmp", new File(path + "/copia.bmp"));   // Guarda la imagen editada en un archivo nuevo.
-        }
-        catch(Exception e)
-        {
-            System.out.println(">: Ocurrió un error(pintar): " + e);
-        }
-    }
     
-    public BufferedImage rotarDyV(BufferedImage img, int a, int b, int c, int d)
+    public BufferedImage unir(BufferedImage B, int a, int b, int c)
+    {
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(int i = 0; i < B.getHeight(); i++)
+        {
+            for(int j = 0; j < B.getWidth(); j++)
+            {
+                queue.add(B.getRGB(i, j));
+            }
+        }
+
+        // ----------------------------------------------------------------
+
+        for(int i = a; i < b; i++)
+        {
+            for(int j = a; j < b; j++)
+            {
+                imagen.setRGB(i, j, queue.remove());
+            }
+        }
+
+        return imagen;
+    }
+
+    public BufferedImage rotarDyV(int a1, int a2, int b1, int b2)
     {
         BufferedImage nueva = null;
         try 
         {
-            File f_imagen = new File(path + "/copia.bmp"); // Obtiene el archivo indicado en path
-            nueva = ImageIO.read(f_imagen);    // Abre el archivo en formato BufferedImage
-
-            if(b - a == 2)   // Si ya es matriz 2x2
+            if(b2 - b1 <= 2)   // Si ya es matriz 2x2
             {
-                int rgb = img.getRGB(0, 0); // a
-                nueva.setRGB(0, 0, rgb);
-
-                rgb = img.getRGB(0, 1);     // b
-                nueva.setRGB(0, 1, rgb);
-
-                rgb = img.getRGB(1, 0);     // c
+                b2--;
+                int rgb = imagen.getRGB(a1, a2); // a  0,0 -> 1,0
                 nueva.setRGB(1, 0, rgb);
 
-                rgb = img.getRGB(1, 1);     // d
+                rgb = imagen.getRGB(a1, b2);     // b  0,1 -> 0,0
+                nueva.setRGB(0, 0, rgb);
+
+                rgb = imagen.getRGB(b2, a1);     // c  1,0 -> 1,1
                 nueva.setRGB(1, 1, rgb);
+
+                rgb = imagen.getRGB(b2, b2);     // d    1,1 -> 0,1
+                nueva.setRGB(0, 1, rgb);
 
                 return nueva;
             }
 
-            BufferedImage A = rotarDyV(img, 0, b/2, c/2, d/2); // Cuadrante Superior Izquierdo
-            BufferedImage B = rotarDyV(img, a/2, 0, c/2, d/2); // Cuadrante Superior Derecho
-            BufferedImage C = rotarDyV(img, a/2, b/2, 0, d/2); // Cuadrante Inferior Izquierdo
-            BufferedImage D = rotarDyV(img, a/2, b/2, c/2, 0); // Cuadrante Inferior Derecho
+            BufferedImage A = rotarDyV(a1, a2, b1, b2/2);
+            BufferedImage B = rotarDyV(a1, b2/2, b1, b2);
+            BufferedImage C = rotarDyV(b2/2, a2, b2/2, b2/2);
+            BufferedImage D = rotarDyV(b2/2, b2/2, b2/2, b2);
+
+            nueva = this.unir(A, a2, b2, b2+1);
+            nueva = this.unir(B, a2, b2, b2+1);
+            nueva = this.unir(C, a2, b2, b2+1);
+            nueva = this.unir(D, a2, b2, b2+1);
 
         } 
         catch (Exception e) 
@@ -122,28 +110,23 @@ public class App
 
     public void rotar2()
     {
-        BufferedImage img = null;
         try 
         {
-            File f_imagen = new File(path + "/copia.bmp"); // Obtiene el archivo indicado en path
-            img = ImageIO.read(f_imagen);    // Abre el archivo en formato BufferedImage
-            BufferedImage nueva = rotarDyV(img, 0, tamaño, tamaño, tamaño);
-            ImageIO.write(nueva, "bmp", new File(path + "/copia.bmp"));   // Guarda la imagen editada en un archivo nuevo.
+            BufferedImage nueva = rotarDyV(0, 0, tamaño, tamaño);
+            ImageIO.write(nueva, "bmp", new File(path + "/DyV.bmp"));   // Guarda la imagen editada en un archivo nuevo.
         
         } 
         catch (Exception e) 
         {
-            System.out.println(">: Ocurrió un error(pintar): " + e);
+            System.out.println(">: Ocurrió un error: " + e);
         }
     }
 
     public static void main(String[] args)
     {
         App usr = new App();
-        if(usr.read())
-        {
-            usr.copiar();
-            usr.rotar();
-        }
+        usr.read();
+        usr.copiar();
+        usr.rotarDyV(0, 0, 0, 1024);  // (0, tamaño, tamaño) [0,0]; [0,4]
     }
 }
